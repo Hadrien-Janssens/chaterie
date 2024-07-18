2924f215f6f44f23d1d86bad52ecffe3d3c4c7d9
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2024-07-18 12:41:47 +0200

    test mise ne page catCard

--- src/components/CatCard.jsx
+++ src/components/CatCard.jsx
@@ -3,37 +3,40 @@ import { CgGenderMale } from "react-icons/cg";
 import Button from "./Button";
 const CatCard = () => {
   return (
-    <div className="bg-[#F4F4F4] flex p-10 gap-3">
-      <div className="basis-[50%] grow shrink-0">
-        <div className="flex flex-col justify-center items-center gap-5 relative  m-auto">
-          <div className="relative z-10">
-            <img
-              className="object-cover h-[150px] w-[150px] shadow-md"
-              src="/urban.jpeg"
-              alt="claire"
-            />
-            <div className="absolute -top-2 -left-2 bg-white w-[100px] h-[100px] -z-10"></div>
-            <div className="absolute -bottom-2 -right-2 bg-white  w-[100px] h-[100px] -z-10"></div>
+    <div className="bg-[#F4F4F4]  p-10 ">
+      <div className="flex gap- justify-center">
+        <div className="basis-[50%] grow shrink-0">
+          <div className="flex flex-col justify-center items-center gap-5 relative  m-auto">
+            <div className="relative z-10">
+              <img
+                className="object-cover h-[150px] w-[150px] shadow-md"
+                src="/urban.jpeg"
+                alt="claire"
+              />
+              <div className="absolute -top-2 -left-2 bg-white w-[100px] h-[100px] -z-10"></div>
+              <div className="absolute -bottom-2 -right-2 bg-white  w-[100px] h-[100px] -z-10"></div>
+            </div>
           </div>
+        </div>
+        <div className="relative flex flex-col">
+          <h3 className="text-[#837067] opacity-[60%] font-extrabold text-lg font-poetsenone">
+            <div className="flex items-center gap-1">
+              Vanille
+              <CgGenderMale className="text-2xl" />
+            </div>
+          </h3>
+          <p className="font-medium mb-10">né le 05/07/2024</p>
           <Button>Voir plus</Button>
+
+          <div className="absolute w-[40%] h-[40%] border-[#B38D7D] border-t-4 border-r-4 -top-5 -right-5"></div>
+          <div className="absolute w-[40%] h-[40%] border-[#B38D7D] border-b-4 border-r-4 -bottom-5 -right-5"></div>
         </div>
       </div>
-      <div className="relative">
-        <h3 className="text-[#837067] opacity-[60%] font-extrabold text-lg font-poetsenone">
-          <div className="flex items-center gap-1">
-            Vanille
-            <CgGenderMale className="text-2xl" />
-          </div>
-        </h3>
-        <p className="font-medium">né le 05/07/2024</p>
-        <p className="text-justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud{" "}
-        </p>
-        <div className="absolute w-[40%] h-[40%] border-[#B38D7D] border-t-4 border-r-4 -top-5 -right-5"></div>
-        <div className="absolute w-[40%] h-[40%] border-[#B38D7D] border-b-4 border-r-4 -bottom-5 -right-5"></div>
-      </div>
+      <p className="text-justify mt-10">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud{" "}
+      </p>
     </div>
   );
 };

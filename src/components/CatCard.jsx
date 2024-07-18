982fd758c06dc560e0e5a1e0ec032c0702dba196
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2024-07-18 12:51:05 +0200

    update style

--- src/components/CatCard.jsx
+++ src/components/CatCard.jsx
@@ -3,9 +3,9 @@ import { CgGenderMale } from "react-icons/cg";
 import Button from "./Button";
 const CatCard = () => {
   return (
-    <div className="bg-[#F4F4F4]  p-10 ">
-      <div className="flex gap- justify-center">
-        <div className="basis-[50%] grow shrink-0">
+    <div className="bg-[#F4F4F4]  p-7 relative">
+      <div className="flex justify-center gap-10">
+        <div className="">
           <div className="flex flex-col justify-center items-center gap-5 relative  m-auto">
             <div className="relative z-10">
               <img
@@ -18,21 +18,21 @@ const CatCard = () => {
             </div>
           </div>
         </div>
-        <div className="relative flex flex-col">
+        <div className=" flex flex-col">
           <h3 className="text-[#837067] opacity-[60%] font-extrabold text-lg font-poetsenone">
             <div className="flex items-center gap-1">
               Vanille
               <CgGenderMale className="text-2xl" />
             </div>
           </h3>
-          <p className="font-medium mb-10">né le 05/07/2024</p>
+          <p className="font-medium mb-6">né le 05/07/2024</p>
           <Button>Voir plus</Button>
 
-          <div className="absolute w-[40%] h-[40%] border-[#B38D7D] border-t-4 border-r-4 -top-5 -right-5"></div>
-          <div className="absolute w-[40%] h-[40%] border-[#B38D7D] border-b-4 border-r-4 -bottom-5 -right-5"></div>
+          <div className="absolute w-[20%] h-[20%] border-[#B38D7D] border-t-4 border-r-4 top-5 right-5"></div>
+          <div className="absolute w-[20%] h-[20%] border-[#B38D7D] border-b-4 border-r-4 bottom-5 right-5"></div>
         </div>
       </div>
-      <p className="text-justify mt-10">
+      <p className="text-justify mt-10 w-11/12 m-auto">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud{" "}

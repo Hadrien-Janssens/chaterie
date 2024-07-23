5ba381c5139c3fbcfc85ec906b74cff7409532d6
BlackPigCompany <86426768+BlackPigCompany@users.noreply.github.com>
2024-07-23 10:12:27 +0200

    lib react-toastify + config du toast + reset du formulaire lors de l'envoi + modification du template pour recuperer les coordonnées

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,33 +3,9 @@ import Button from "./components/Button";
 import TitleSection from "./components/TitleSection";
 import Footer from "./components/Footer";
 import { NavLink } from "react-router-dom";
-import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
+import Contact from "./components/Contact";
 
 const Home = () => {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        import.meta.env.VITE_EMAIL_JS_SERVICE,
-        "template_d8ibw66",
-        form.current,
-        {
-          publicKey: import.meta.env.VITE_EMAIL_JS_PUBLIC_KEY,
-        }
-      )
-      .then(
-        () => {
-          console.log("SUCCESS!");
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
-  };
   return (
     <>
       <Nav />
@@ -154,54 +130,7 @@ const Home = () => {
           </div>
         </div> */}
         {/* CONTACT  */}
-        <div className="m-5 relative mt-20">
-          <img
-            src={"/chat-rogolo.png"}
-            width={150}
-            height={150}
-            alt="dessin de chat rigolo"
-            className="absolute -top-16"
-          />
-          <div className="bg-[#97918D] p-3 relative">
-            <div className="absolute border-[#B38D7D] opacity-[67%] border-b-4 border-l-4 w-32 h-full -bottom-3 -left-3"></div>
-            <h3 className="text-white text-2xl font-bold mb-3">
-              Contactez-nous
-            </h3>
-            <form
-              ref={form}
-              onSubmit={sendEmail}
-              className="flex flex-col gap-3 w-10/12 m-auto "
-            >
-              <div className="flex justify-between">
-                <label htmlFor="name" className="text-white">
-                  Nom & Prénom
-                </label>
-                <input type="text" id="name" name="user_name" />
-              </div>
-              <div className="flex justify-between">
-                <label htmlFor="email" className="text-white">
-                  Email
-                </label>
-                <input type="email" name="user_email" id="email" />
-              </div>
-
-              <div className="flex justify-between">
-                <label htmlFor="message" className="text-white">
-                  Message
-                </label>
-                <textarea
-                  name="message"
-                  id="message"
-                  placeholder="Votre message"
-                ></textarea>
-              </div>
-              <div className="text-black">
-                <Button onSubmit={sendEmail}>Envoyer</Button>
-              </div>
-            </form>
-            <div className="absolute border-[#B38D7D] opacity-[67%] border-t-4 border-r-4 w-32 h-full -top-3 -right-3"></div>
-          </div>
-        </div>
+        <Contact />
         {/* NOUS TROUVER  */}
         <div className="flex bg-[#51687F] m-5 mt-12 p-3 items- justify-between relative">
           <div className="basis-[50%] grow">
